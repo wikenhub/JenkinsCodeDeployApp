98fbdd7692401a930b33ca1af5d17615820a2810
wikenhub <69572715+wikenhub@users.noreply.github.com>
2024-04-11 16:20:59 +0900

    Update HelloWorldController.java

--- src/main/java/com/wiken/e2edemo/HelloWorldController.java
+++ src/main/java/com/wiken/e2edemo/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@GetMapping("/hello")
     public String sayHello() {
-        return "Hi. Welcome to CNA cloud apps - CodeDeploy By Jenkins -v1.0";
+        return "Hi. Welcome to CNA cloud apps - CodeDeploy By Jenkins -v2.0";
     }
 
 }
