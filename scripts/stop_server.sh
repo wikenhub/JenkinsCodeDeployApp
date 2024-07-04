ca213b077917ec54485b8ce8440b8ac7a7fc5344
wikenhub <69572715+wikenhub@users.noreply.github.com>
2024-07-04 22:56:14 +0900

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 isExistApp=`pgrep java`
 if [[ -n  $isExistApp ]]; then
-   /home/ec2-user/apache-tomcat-9.0.85/bin/shutdown.sh
+   /home/ec2-user/apache-tomcat-9.0.90/bin/shutdown.sh
 fi
